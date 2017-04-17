dd4fae6d65e592d3841eb65610996483a37ba940
gumyns <gumyns@gmail.com>
2017-04-17 17:33:33 +0200

    make fix

--- server/scripts/dependency/41_build_zeromq.py
+++ server/scripts/dependency/41_build_zeromq.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 import dep_lib
 import sys
+import time
 import os
 from subprocess import call
+from subprocess import Popen
 
 dependencyName = 'build_zeromq'
 dependencyVersion = "4.2.0"
@@ -17,4 +19,7 @@ srcDir = os.path.join(directory, "zeromq")
 print "Boostraping {0}-{1}...".format(dependencyName, dependencyVersion)
 call(["./autogen.sh", ''], cwd = srcDir)
 call(["./configure", ''], cwd = srcDir)
-call(["make", ''], cwd = srcDir)
+time.sleep(.5)
+print srcDir
+make_process = Popen("make", cwd = srcDir)
+make_process.wait()
