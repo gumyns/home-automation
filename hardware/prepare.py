53ff0c832d9682e1aa70351a15ead5cd0c4060e6
gumyns <gumyns@gmail.com>
2017-06-26 19:57:58 +0200

    download flashing tool

--- hardware/prepare.py
+++ hardware/prepare.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python
 import os
 import sys
+from subprocess import call
 
+# toolchain defines
 libs = [
     os.path.expanduser("~") + '/opt/STM32/STM32Cube_FW_F0_V1.8.0'
 ]
 
 devices = [
-    ['STM32F030x6', libs[0]],
-    ['STM32F042x6', libs[0]]
+    'STM32F030x6',
+    'STM32F042x6'
 ]
-
-scripts = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), "scripts")
+# utils
 currentDir = os.path.dirname(os.path.realpath(sys.argv[0]))
+scriptsDir = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), "scripts")
 
-for device in devices:
-    os.system("python " + os.path.join(scripts, "generateToolchain.py") + " " + currentDir + " " + device[0])
+# stm32flash download and compile
+stm32flashDir = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), "stm32flash")
+stm32flashBinary = os.path.join(stm32flashDir, "stm32flash")
 
+if not os.path.isdir(stm32flashDir):
+    os.system("git clone http://git.code.sf.net/p/stm32flash/code stm32flash")
+if not os.path.exists(stm32flashBinary):
+    call("make", cwd = stm32flashDir)
+
+# toolchain generator
 for device in devices:
-    if not os.path.isdir(device[1]):
-        print "Directory {0} doesn't exists. Skipping".format(device[1])
+    os.system("python " + os.path.join(scriptsDir, "generateToolchain.py") + " " + currentDir + " " + device)
+
+# cmake generator
+for lib in libs:
+    if not os.path.isdir(lib):
+        print "Directory {0} doesn't exists. Skipping".format(lib)
